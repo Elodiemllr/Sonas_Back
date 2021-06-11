b894a076757ac805f2932f9048d83b64d7595aa4
elodiemllr <elodie.muller.pro@outlook.com>
2021-06-11 14:21:29 +0200

    router file

---
+++ app/router.js
@@ -0,0 +1,12 @@
+//Require router from express
+const { Router } = require('express');
+const router = Router();
+
+/**
+ * A module representing a router
+ * @export router
+ */
+
+
+
+ module.exports = router;
